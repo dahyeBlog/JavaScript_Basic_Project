3269d0d9b2d4e075227f330fe6e7bd32a86d622f
dahye <dhk8233@gmail.com>
2022-11-03 11:34:34 +0900

    menu

--- menu/app.js
+++ menu/app.js
@@ -72,3 +72,77 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector('.btn-container')
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu)
+  displayMenuButtons()
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons(){
+  const categories = menu.reduce(
+    function(values, item){
+      if(!values.includes(item.category)){
+        values.push(item.category)
+      }
+      return values      
+    },["all"])
+    // console.log(categories);
+    
+    const categoryBtns = categories.map(function(category){
+      // console.log(category);
+      return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`
+    })
+    .join('')
+
+    btnContainer.innerHTML = categoryBtns
+    const filterBtns = btnContainer.querySelectorAll('.filter-btn')
+    // console.log(filterBtns);
+    
+    filterBtns.forEach(function(btn){
+      btn.addEventListener('click', function(e){
+        // console.log(e.currentTarget.dataset.id);
+        const category = e.currentTarget.dataset.id
+        const menuCategory = menu.filter(function(menuItem){
+          if(menuItem.category === category){
+            return menuItem
+            // console.log(menuItem);
+          }
+        })
+        
+        if(category === 'all'){
+          displayMenuItems(menu)
+        } else {
+          displayMenuItems(menuCategory)
+        }
+      
+      })
+    })
+
+  }
+
