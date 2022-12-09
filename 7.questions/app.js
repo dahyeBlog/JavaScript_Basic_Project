c61570639c6dccadc057b4c5e8cbaaafc13fd99e
dahye <dhk8233@gmail.com>
2022-12-09 09:16:25 +0900

    toggle버튼 사용

--- 7.questions/app.js
+++ 7.questions/app.js
@@ -1,20 +1,22 @@
-//using selectors inside the element
-// traversing the dom
 const questions = document.querySelectorAll('.question')
 
-questions.forEach(function (question){
+// console.log(questions);
+
+
+questions.forEach((question) => {
   const btn = question.querySelector('.question-btn')
   // console.log(btn);
-  
-  btn.addEventListener('click', function(){
-    // console.log(question);
-    
-    questions.forEach(function (item){
-      if(item !== question){
+
+  btn.addEventListener('click', () => {
+
+    questions.forEach((item) => {
+      if(item !== question) {
         item.classList.remove('show-text')
       }
       
     })
-    question.classList.toggle("show-text");
+
+    question.classList.toggle('show-text')
   })
+  
 })
