7020fde8b50532927d58b6196dd7693307632261
dahye <dhk8233@gmail.com>
2022-12-21 23:29:58 +0900

    cocktail_search

--- cocktails/src/setDrink.js
+++
@@ -1,9 +0,0 @@
-
-const setDrink = (section) => {
-  section.addEventListener('click', function(e) {
-  const id = e.target.parentElement.dataset.id;
-  localStorage.setItem('drink', id)    
-  })  
-}
-
-export default setDrink
