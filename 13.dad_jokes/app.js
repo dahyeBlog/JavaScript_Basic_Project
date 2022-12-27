b46112daab1bf91a2004210c4265c9a5acabe6b0
dahye <dhk8233@gmail.com>
2022-12-27 14:37:20 +0900

    dadJokeFetchApi

--- 13.dad_jokes/app.js
+++ 13.dad_jokes/app.js
@@ -11,21 +11,24 @@ const fetchDadJoke = async () => {
   result.textContent = 'Loading...'
   try {
     const response = await fetch(url, {
-      headers:{
+      headers: {
         Accept: 'application/json',
-        'User-Agent': 'learning app',
-      }
+        'User-Agent': 'learning app'
+      },
     })
-    if(!response.ok){
-      throw new Error(' Error')
+
+    if (!response.ok) {
+      throw new Error('error')
     }
-   
+
     const data = await response.json()
-    
+
+      console.log(data.joke);
+      
     result.textContent = data.joke
   } catch (error) {
     console.log(error.message);
-    result.textContent = 'There was an error...';
+    result.textContent = '에러가 발생했습니다. '
   }
 }
 
